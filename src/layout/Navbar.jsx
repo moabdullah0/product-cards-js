6548977e379b51cbae2bc760fb85b390beddda8e
moabdullah0 <mohamad.alibrahim1998@gmail.com>
2025-03-04 12:24:14 +0300

    fix navbar response

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Drawer from "./Drawer";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -21,11 +27,11 @@ const Navbar = () => {
             </span>
           </a>
           <button
-            data-collapse-toggle="navbar-default"
+            onClick={toggleMenu}
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -37,14 +43,14 @@ const Navbar = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
           </button>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <Link 
@@ -79,12 +85,11 @@ const Navbar = () => {
                   Pricing
                 </a>
               </li>
-               <div className="flex gap-3 items-center justify-center">
-               <Drawer/>
-               <Link className="text-white bg-blue-700 py-2 px-4 rounded-lg" to={'/login'}>Login</Link>
-               </div>
+              <div className="flex gap-3 items-center justify-center">
+                <Drawer/>
+                <Link className="text-white bg-blue-700 py-2 px-4 rounded-lg" to={'/login'}>Login</Link>
+              </div>
               <li>
-               
               </li>
             </ul>
           </div>
